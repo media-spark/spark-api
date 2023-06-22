41680d4a1b83090a7810ff44be8f07b81caad1d1
Andy Randrianirina <andy.randrianirina@gmail.com>
2023-06-22 21:21:31 +0200

    Config vercel API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  console.log('################################ Spark API server starting');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
 bootstrap();

c50c42b232861278c1836856ae0ed1a517f5e964
Fynaritra <finaritra.mickaelle@gmail.com>
2023-06-24 18:51:00 +0400

    Revert "Resolve conflicts"
    This reverts commit 27449e7dacee2da981c16b7a33491a3d41fdf6a1, reversing
    changes made to ba2a953960a927f464632761ef4127392c7d0637.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoService } from './demo/demo.service';
 import { DemoController } from './demo/demo.controller';
-import { ProofOfConceptModule } from './proof-of-concept/proof-of-concept.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import * as dotenv from 'dotenv';
 import config from './database.config';
-
-dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
-    TypeOrmModule.forFeature([Demo]),
-    
-    MongooseModule.forRoot(`${process.env.MONGODB_URI}`),
-    ProofOfConceptModule,
+    TypeOrmModule.forFeature([Demo])
   ],
   controllers: [AppController, DemoController],
   providers: [AppService, DemoService],
-}) 
+})
 export class AppModule {}
